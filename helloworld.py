6db2fb554fb2e0c8af9e8767d3f0859faa1837b4
Ezkll <151662934+Ezkll@users.noreply.github.com>
2024-02-07 16:49:08 -0700

    Hello World

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+
+def hello_world(name: str) -> str:
+    ''' This is the hello_world Docstring '''
+    print("Hello World")
+    print("Still in this block")
+    
+print("Outside of the block")
+
+single = "single \
+line"
+
+triples = '''"Triple Quotes
+
+Are the Best"'''
+    
+print(triples)
+
+#example: int
+#print(example)
+#ugh
